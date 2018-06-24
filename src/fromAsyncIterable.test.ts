d9ec63dcabb8d28160dc96fa0881c9b54b6ae09f
pinyin <bob432091@gmail.com>
2018-06-24 21:42:49 +0800

    update test of fromAsyncIterable

--- src/fromAsyncIterable.test.ts
+++ src/fromAsyncIterable.test.ts
@@ -1,6 +1,6 @@
 import 'babel-polyfill'
 import {timer} from 'rxjs'
-import {takeUntil, toArray} from 'rxjs/operators'
+import {mapTo, takeUntil, toArray} from 'rxjs/operators'
 import {fromAsyncIterable} from './fromAsyncIterable'
 
 describe(`${fromAsyncIterable.name}`, () => {
@@ -10,13 +10,14 @@ describe(`${fromAsyncIterable.name}`, () => {
         yield 1
         await timer(300).toPromise()
         yield 2
-        yield timer(300).toPromise()
+        yield timer(300).pipe(mapTo(3)).toPromise()
+        yield* [4]
         finished++
     }
 
     test(`should convert async iterable to observable`, async () => {
         const result = await fromAsyncIterable(generator()).pipe(toArray()).toPromise()
-        expect(result).toEqual([1, 2, 0])
+        expect(result).toEqual([1, 2, 3, 4])
         expect(finished).toEqual(1)
     })
 
