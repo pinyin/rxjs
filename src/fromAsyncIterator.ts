620c8512ccbfe25814dab2b80f5467581fc0325c
pinyin <bob432091@gmail.com>
2018-07-01 15:46:05 +0800

    clean code

--- src/fromAsyncIterator.ts
+++ src/fromAsyncIterator.ts
@@ -4,13 +4,10 @@ export function fromAsyncIterator<T>(iterator: AsyncIterator<T>): Observable<T> 
     return Observable.create((subscriber: Subscriber<T>): Unsubscribable => {
         async function forwardValues(): Promise<void> {
             try {
-                do {
-                    const next = await iterator.next()
-                    if (next.done) {
-                        break
-                    }
+                for (let next = await iterator.next(); !subscriber.closed && !next.done;) {
                     subscriber.next(next.value)
-                } while (!subscriber.closed)
+                    next = await iterator.next()
+                }
                 subscriber.complete()
             } catch (e) {
                 subscriber.error(e)
