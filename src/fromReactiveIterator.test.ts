3bc8752296dc181bfa558cf0f4f6397afdfef368
pinyin <bob432091@gmail.com>
2018-07-01 15:51:22 +0800

    add fromReactiveIterator

---
+++ src/fromReactiveIterator.test.ts
@@ -0,0 +1,29 @@
+import {Observable, timer} from 'rxjs'
+import {mapTo, startWith, takeUntil, tap, toArray} from 'rxjs/operators'
+import {fromReactiveIterator} from './fromReactiveIterator'
+
+describe(`${fromReactiveIterator.name}`, () => {
+    let finished = 0
+    function* generator(): Iterator<number | Observable<any>> {
+        yield 1
+        yield timer(100)
+        yield 2
+        yield timer(100).pipe(startWith(1))
+        yield 3
+        const d: 4 = yield timer(100).pipe(mapTo(4))
+        yield d
+        yield timer(100).pipe(tap(() => finished++))
+    }
+
+    it(`should convert iterator to observable`, async () => {
+        const result = fromReactiveIterator(generator()).pipe(toArray()).toPromise()
+        expect(await result).toEqual([1, 2, 3, 4])
+        expect(finished).toEqual(1)
+    })
+
+    it(`should terminate unfinished observable when subscribed`, async () => {
+        const result = fromReactiveIterator(generator()).pipe(takeUntil(timer(350)), toArray()).toPromise()
+        expect(await result).toEqual([1, 2, 3, 4])
+        expect(finished).toEqual(1)
+    })
+})
---
+++ src/fromReactiveIterator.ts
@@ -0,0 +1,51 @@
+import {existing} from '@pinyin/maybe'
+import {something} from '@pinyin/types'
+import {isObservable, Observable, Subject, Subscriber, Subscription, TeardownLogic} from 'rxjs'
+import {takeUntil} from 'rxjs/operators'
+import {Pulse} from './Pulse';
+
+export function fromReactiveIterator<T extends Exclude<something, Observable<any>>>(
+    iterator: Iterator<T | Observable<any>>
+): Observable<T> {
+    return Observable.create((subscriber: Subscriber<T>): TeardownLogic => {
+        const subscription = new Subscription()
+        const closeSignal = new Subject()
+
+        const closeIterator = closeSignal.subscribe(() => {
+            if (existing(iterator.return)) {
+                iterator.return()
+            }
+        })
+        subscription.add(closeIterator)
+
+        async function forwardValues(): Promise<void> {
+            try {
+                for (let next = iterator.next(); !subscriber.closed && !next.done;) {
+                    if (isObservable<any>(next.value)) {
+                        const value = await next.value.pipe(takeUntil(closeSignal)).toPromise()
+                        next = iterator.next(value)
+                    } else {
+                        subscriber.next(next.value as T) // TODO why is type cast necessary?
+                        next = iterator.next()
+                    }
+                }
+                subscriber.complete()
+            } catch (e) {
+                subscriber.error(e)
+            }
+        }
+
+        forwardValues()
+
+        return () => {
+            subscription.unsubscribe()
+            closeSignal.next(Pulse)
+        }
+    })
+}
+
+export class Closed extends Error {
+    constructor() {
+        super(`Reactive iterator closed.`)
+    }
+}
